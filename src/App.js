3ccfd5de09bd234a7e21c9e7d693bb1ff19056ca
Muhammad Suleman <54745700+Suleman97@users.noreply.github.com>
2021-06-26 22:43:06 +0500

    trying to understand react Suspense

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { Suspense, useEffect, useState } from 'react';
 import './App.css';
 import Dinner from './components/Dinner';
 // import Child from './components/Child';
@@ -13,6 +13,7 @@ function App() {
 
   useEffect(() => {
     async function fetchData() {
+
       const api = await fetch(`https://api.github.com/users/Suleman97/repos`);
       const data = await api.json();
       // console.log(data);
@@ -21,7 +22,7 @@ function App() {
     fetchData();
   }, []);
 
-  console.log(repos)
+  console.log(repos);
   return (
     <div className="App">
       <Dinner dish="Chicken Biryani" />
@@ -32,11 +33,15 @@ function App() {
       <CounterContext.Provider value={countState}>
         <Parent />
       </CounterContext.Provider>
+
       <ul>
-        {repos.map((Obj, index) => (
-          <li key={index}>{Obj.name}</li>
-        ))}
+        <Suspense fallback={<h1>Loading...</h1>}>
+          {repos.map((Obj, index) => (
+            <li key={index}>{Obj.name}</li>
+          ))}
+        </Suspense>
       </ul>
+
     </div>
   );
 }
