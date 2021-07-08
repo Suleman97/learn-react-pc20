167e266624686f56945c40d3b6c383bb95574001
Muhammad Suleman <54745700+Suleman97@users.noreply.github.com>
2021-07-08 20:56:35 +0500

    understanding react router dom

---
+++ src/Route.js
@@ -0,0 +1,13 @@
+
+import { Link, Route, Switch, BrowserRouter } from 'react-router-dom'
+
+
+function RouterConfig() {
+  return (
+    <div>
+      <h1>This is router</h1>
+    </div>
+  )
+}
+
+export default RouterConfig;
